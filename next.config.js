10e9f9455c35477f900a62f42f5c0b527832f65a
ProdigySim <prodigysim@gmail.com>
2021-08-05 09:40:31 -0500

    Images work

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 // This file is not going through babel transformation.
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
 const debug = process.env.NODE_ENV !== 'production'
 
-module.exports = {
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
-}
+module.exports = withPlugins([optimizedImages, {
+  optimizeImagesInDev: true,
+}], {
+  assetPrefix: !debug ? '/michael_busbies/' : '',
+  images: {
+    disableStaticImages: true,
+  },
+});
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,25 +1,24 @@
-import Image from 'next/image'
 
 // css
-import '../css/resume.module.scss';
-
-// imgs
-import bvLogoUrl from '../images/resume/bv-logo.jpg';
-import curalateLogoUrl from '../images/resume/curalate-logo.png';
-import bevyupLogoUrl from '../images/resume/bevyup-logo.png';
+import styles from './resume.module.scss';
 
 export default function Resume() {
   return (
     <main className="resume">
       <h1>Michael Busby</h1>
       <p> I'm a full-stack Software Developer, with a focus on front-end systems.
-          I've been working fully remote since 2018, after moving from Seattle to Kansas City.
-          I have a passion for security and user-centered design. Developers are users, too!
+        I've been working fully remote since 2018, after moving from Seattle to Kansas City.
+        I have a passion for security and user-centered design. Developers are users, too!
       </p>
       <hr />
       <h2>Salaried positions</h2>
-      <h3>BazaarVoice Inc. - Senior Staff Software Engineer</h3>
-      <h4>August 2020 – May 2021 (10 months)</h4>
+      <div className={styles.companyHeader}>
+        <div>
+          <h3>BazaarVoice Inc. - Senior Staff Software Engineer</h3>
+          <h4>August 2020 – May 2021 (10 months)</h4>
+        </div>
+        <img className={styles.logo} src={require('../images/resume/bv-logo.jpg')} alt="BazaarVoice Logo" />
+      </div>
       <p>Continuation of my Curalate Software Engineering role, with some additional responsibilities relating to the acquisition.</p>
       <p>Collaborated with existing BV teams in the US & abroad on unifying the Identity & Login systems between Curalate and BV.</p>
       <p>Worked on documentation, tech debt paydown, and mentorship in order to knowledge & ownership transfer for:
@@ -30,14 +29,18 @@ export default function Resume() {
           <li>Frontend architecture and infrastructure</li>
         </ul>
       </p>
-      <h3>Curalate (Acquired by BazaarVoice) - Software Engineer III</h3>
-      <h4>November 2015 to August 2020 (4 yrs 10 months) </h4>
-      <div style={{float: 'left', width: '100px', height: '100px' }}><Image className="logo" src={curalateLogoUrl} alt="Curalate Logo" /></div>
+      <div className={styles.companyHeader}>
+        <div>
+          <h3>Curalate (Acquired by BazaarVoice) - Software Engineer III</h3>
+          <h4>November 2015 to August 2020 (4 yrs 10 months) </h4>
+        </div>
+        <img className={styles.logo} src={require('../images/resume/curalate-logo.png')} alt="Curalate Logo" />
+      </div>
       <p>Full stack problem solver at a roller coaster of a startup. We built smart tools to help brands blend social and ecommerce end-to-end!</p>
       <ul className="emoji-bulleted">
         <li>💪 Full-Stack Developer on web+mobile applications on Scala+JS stack.</li>
         <li>💅 Built our first Design Pattern Library for the company in AngularJs/CSS.</li>
-        <li>🔒 AppSec focused: 
+        <li>🔒 AppSec focused:
           <ul>
             <li>Built Authentication & CSRF schemes; integrated Cognito IdP and JWT validation.</li>
             <li>Ran Pen-Testing, built XSS, SSRF, CSRF mitigations, secure coding patterns, and developer education.</li>
@@ -56,8 +59,13 @@ export default function Resume() {
         <li>❤️ Learned user empathy and collaborative developer culture with teams spread across the US.</li>
         <li>😲 Watched mountains move and helped hack solutions to move them 😉</li>
       </ul>
-      <h3>BevyUp (Acquired by Nordstrom) - Software Developer</h3>
-      <h4>August 2012 to April 2015 (2 years 9 months)</h4>
+      <div className={styles.companyHeader}>
+        <div>
+          <h3>BevyUp (Acquired by Nordstrom) - Software Developer</h3>
+          <h4>August 2012 to April 2015 (2 years 9 months)</h4>
+        </div>
+        <img className={styles.logo} src={require('../images/resume/bevyup-logo.png')} alt="BevyUp Logo" />
+      </div>
       <p>Employee no. 7 at a small startup. We enabled ecommerce customers to browse online stores together, with rich interactivity, screen sharing, and video chat--all within a single JS tag!</p>
       <ul className="emoji-bulleted">
         <li>😅 Lead Frontend Developer on a responsive co-browsing web application.</li>
